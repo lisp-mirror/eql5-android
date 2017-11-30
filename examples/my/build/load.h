feab624cd3d159342adf0cfac512f90b82172024
polos <polos.ruetz@gmail.com>
2017-11-30 09:33:21 +0100

    better formatting of JNI Java code

--- examples/my/build/load.h
+++ examples/my/build/load.h
@@ -12,41 +12,37 @@ class Qt_EQL_Application : public QApplication {
 public:
     Qt_EQL_Application(int argc, char** argv) : QApplication(argc, argv) {}
 
-    // stolen from: https://www.kdab.com/qt-on-android-how-to-restart-your-application/
+    // JNI (Java Native Interface) is very verbose, but the only practical way to call Java from Qt
 
-    Q_INVOKABLE void restartApp() { // callable from Lisp
+    Q_INVOKABLE void restartApp() {
+
+        // stolen from: https://www.kdab.com/qt-on-android-how-to-restart-your-application/
 
         auto activity = QtAndroid::androidActivity();
 
         auto packageManager = activity.callObjectMethod(
-            "getPackageManager",
-            "()Landroid/content/pm/PackageManager;");
+            "getPackageManager", "()Landroid/content/pm/PackageManager;");
          
         auto activityIntent = packageManager.callObjectMethod(
-            "getLaunchIntentForPackage",
-            "(Ljava/lang/String;)Landroid/content/Intent;",
-            activity.callObjectMethod("getPackageName",
-            "()Ljava/lang/String;").object());
+            "getLaunchIntentForPackage", "(Ljava/lang/String;)Landroid/content/Intent;",
+            /*L*/ activity.callObjectMethod("getPackageName", "()Ljava/lang/String;").object());
          
         auto pendingIntent = QAndroidJniObject::callStaticObjectMethod(
-            "android/app/PendingIntent", "getActivity",
-            "(Landroid/content/Context;ILandroid/content/Intent;I)Landroid/app/PendingIntent;",
-            activity.object(), jint(0), activityIntent.object(),
-            QAndroidJniObject::getStaticField<jint>("android/content/Intent",
-            "FLAG_ACTIVITY_CLEAR_TOP"));
+            "android/app/PendingIntent",
+            "getActivity", "(Landroid/content/Context;ILandroid/content/Intent;I)Landroid/app/PendingIntent;",
+            /*L*/  activity.object(),
+            /*IL*/ jint(0), activityIntent.object(),
+            /*I*/  QAndroidJniObject::getStaticField<jint>("android/content/Intent", "FLAG_ACTIVITY_CLEAR_TOP"));
          
         auto alarmManager = activity.callObjectMethod(
-            "getSystemService",
-            "(Ljava/lang/String;)Ljava/lang/Object;",
-            QAndroidJniObject::getStaticObjectField("android/content/Context",
-            "ALARM_SERVICE",
-            "Ljava/lang/String;").object());
+            "getSystemService", "(Ljava/lang/String;)Ljava/lang/Object;",
+            /*L*/ QAndroidJniObject::getStaticObjectField("android/content/Context", "ALARM_SERVICE", "Ljava/lang/String;").object());
          
         alarmManager.callMethod<void>(
-            "set",
-            "(IJLandroid/app/PendingIntent;)V",
-            QAndroidJniObject::getStaticField<jint>("android/app/AlarmManager", "RTC"),
-            jlong(QDateTime::currentMSecsSinceEpoch() + 1000), pendingIntent.object()); } // wait 1 sec
+            "set", "(IJLandroid/app/PendingIntent;)V",
+            /*IJL*/ QAndroidJniObject::getStaticField<jint>("android/app/AlarmManager", "RTC"),
+                    jlong(QDateTime::currentMSecsSinceEpoch() + 1000), // wait 1 sec (for slow devices)
+                    pendingIntent.object()); }
 };
 
 QT_END_NAMESPACE
